ccc5964a8aa636491d32d8f08026f3dc18727ab3
Tony Eneh <aeneh90@gmail.com>
2019-10-30 04:04:14 +0100

    import map operator properly

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { map } from 'rxjs/operators/map';
+import map from 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
@@ -11,7 +11,5 @@ export class ProductService {
   getAlbum(id: number) {
 return this._http.get(this._albumUrl).pipe(map((response => response.json())));
   }
-
-
 
 }
