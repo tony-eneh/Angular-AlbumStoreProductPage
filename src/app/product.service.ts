cb0d97b9fe735f8722271d0550971518b3ee18ea
Tony Eneh <aeneh90@gmail.com>
2019-10-30 04:25:15 +0100

    Fix: properly cast album observables in product service

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -11,7 +11,7 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album> {
-return this._http.get(this._albumUrl).pipe(map((response => response.json() as Album )));
+return this._http.get(this._albumUrl).pipe(map((response => response.json() as Observable<Album> )));
   }
 
 }
