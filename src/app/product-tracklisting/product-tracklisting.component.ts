6aa6fd73cb1bacfa7447e1677d493d26a5c0030b
Tony Eneh <aeneh90@gmail.com>
2019-10-30 04:59:19 +0100

    Import product service and album interface in product-tracklisting component

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Album } from '../album';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-tracklisting',
