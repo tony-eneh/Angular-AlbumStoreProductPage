15e18798280cacce8b1c0d8f9f82945a6c7054d9
Tony Eneh <aeneh90@gmail.com>
2019-10-30 05:44:15 +0100

    Declare routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { ProductDescriptionComponent } from './product-description/product-descr
 import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes = [{path: 'products', component: ProductListComponent}, {path: 'product/:id', component: ProductPageComponent}];
 
 @NgModule({
   declarations: [
@@ -21,7 +24,8 @@ import { ProductListComponent } from './product-list/product-list.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
