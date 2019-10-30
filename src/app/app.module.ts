e8d41b6ff8656b57cc914d9cb7d5b305217af634
Tony Eneh <aeneh90@gmail.com>
2019-10-30 05:59:03 +0100

    Implement routing for different urls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,11 @@ import { ProductTracklistingComponent } from './product-tracklisting/product-tra
 import { ProductListComponent } from './product-list/product-list.component';
 import { RouterModule, Routes } from '@angular/router';
 
-const appRoutes = [{path: 'products', component: ProductListComponent}, {path: 'product/:id', component: ProductPageComponent}];
+const appRoutes = [
+  {path: 'products', component: ProductListComponent},
+  {path: 'product/:id', component: ProductPageComponent},
+  {path: '', redirectTo: 'products', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
