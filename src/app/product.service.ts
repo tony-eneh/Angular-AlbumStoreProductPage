6ac4fbd0ed88ea5bd4aedb60cbf3c7b382be3134
Tony Eneh <aeneh90@gmail.com>
2019-10-30 03:37:53 +0100

    Create private _albumUrl property in product service

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 @Injectable()
 export class ProductService {
-
-  constructor() { }
+  
+  _albumUrl = '../assets/album.json';
+  
+  constructor(private _http: Http) { }
 
 }
